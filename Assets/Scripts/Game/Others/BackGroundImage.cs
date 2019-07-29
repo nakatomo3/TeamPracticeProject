b3a124413f45e2f0d64ea5e8f9c3cb57a08551c4
sugimotokento <keyunutoma@gmail.com>
2019-07-29 14:33:32 +0900

    道路と背景を生成するようにした

--- Assets/Scripts/Game/Others/BackGroundImage.cs
+++ Assets/Scripts/Game/Others/BackGroundImage.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BackGroundImage : MonoBehaviour
 {
+    private Transform thisTransform;
+    private float destroyCounter = 0;
+    private float SCROLL_SPEED = 2.5f;
+    private float speed;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        thisTransform = this.transform;
+        speed = SCROLL_SPEED;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (SystemManager.instance.GetRunning() == false) {
+            // speed = SCROLL_SPEED * (1 + SpeedManager.instance.GetSpeed() / 50);/////////////////////////////////
+            thisTransform.Translate(0, -speed * Time.deltaTime, 0);
+
+            destroyCounter += Time.deltaTime;
+            if (destroyCounter >= 40) {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
